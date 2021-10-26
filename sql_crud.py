f57ec7301801af69c85503b1c5ece458970ec4cc
domsq <Domsq@hotmail.com>
2021-10-26 10:56:23 +0000

    Add table FaveCars to Chinook DB and create some records

--- sql_crud.py
+++ sql_crud.py
@@ -20,6 +20,16 @@ class Programmer(base):
     famous_for = Column(String)
 
 
+class FaveCars(base):
+    """Create a table for some of Dom's favourite cars"""
+    __tablename__ = "FaveCars"
+    CarId = Column(Integer, primary_key=True)
+    Make = Column(String)
+    Model = Column(String)
+    Engine = Column(String)
+    BHP = Column(String)
+
+
 # Define a session, instead of direct DB connection
 # Create new instance of sessionmaker, then point to the DB
 Session = sessionmaker(db)
@@ -28,63 +38,113 @@ session = Session()
 # Create DB using declarative_base subclass
 base.metadata.create_all(db)
 
+# Create records in "FaveCars" table
+am_db9 = FaveCars(
+    Make="Aston Martin",
+    Model="DB9",
+    Engine="5.9L V12",
+    BHP="510"
+)
+
+porsche_turbo = FaveCars(
+    Make="Porsche",
+    Model="996 Turbo",
+    Engine="3.6L Flat-6 Twin-Turbo",
+    BHP="414"
+)
+
+ford_mustang = FaveCars(
+    Make="Ford",
+    Model="Mustang (2016)",
+    Engine="5L V8",
+    BHP="435"
+)
+
+mclaren_senna = FaveCars(
+    Make="McLaren",
+    Model="Senna",
+    Engine="4L V8 Twin-Turbo",
+    BHP="789"
+)
+
+ferrari_f40 = FaveCars(
+    Make="Ferrari",
+    Model="F40",
+    Engine="2.9L V8 Twin-Turbo",
+    BHP="471"
+)
+
+merc_sl65 = FaveCars(
+    Make="Mercedes-Benz",
+    Model="SL65 AMG",
+    Engine="6L V12 Twin-Turbo",
+    BHP="604"
+)
+
+# Add car instances to session
+# session.add(am_db9)
+# session.add(porsche_turbo)
+# session.add(ford_mustang)
+# session.add(mclaren_senna)
+# session.add(ferrari_f40)
+# session.add(merc_sl65)
 
 # Create records on "Programmer" table
-ada_lovelace = Programmer(
-    first_name="Ada",
-    last_name="Lovelace",
-    gender="F",
-    nationality="British",
-    famous_for="First Ever Programmer"
-)
+# ada_lovelace = Programmer(
+#     first_name="Ada",
+#     last_name="Lovelace",
+#     gender="F",
+#     nationality="British",
+#     famous_for="First Ever Programmer"
+# )
 
-alan_turing = Programmer(
-    first_name="Alan",
-    last_name="Turing",
-    gender="M",
-    nationality="British",
-    famous_for="Modern Computing"
-)
+# alan_turing = Programmer(
+#     first_name="Alan",
+#     last_name="Turing",
+#     gender="M",
+#     nationality="British",
+#     famous_for="Modern Computing"
+# )
 
-grace_hopper = Programmer(
-    first_name="Grace",
-    last_name="Hopper",
-    gender="F",
-    nationality="American",
-    famous_for="COBOL Language"
-)
+# grace_hopper = Programmer(
+#     first_name="Grace",
+#     last_name="Hopper",
+#     gender="F",
+#     nationality="American",
+#     famous_for="COBOL Language"
+# )
 
-margaret_hamilton = Programmer(
-    first_name="Margaret",
-    last_name="Hamilton",
-    gender="F",
-    nationality="American",
-    famous_for="Apollo 11"
-)
+# margaret_hamilton = Programmer(
+#     first_name="Margaret",
+#     last_name="Hamilton",
+#     gender="F",
+#     nationality="American",
+#     famous_for="Apollo 11"
+# )
 
-bill_gates = Programmer(
-    first_name="Bill",
-    last_name="Gates",
-    gender="M",
-    nationality="American",
-    famous_for="Microsoft Founder"
-)
+# bill_gates = Programmer(
+#     first_name="Bill",
+#     last_name="Gates",
+#     gender="M",
+#     nationality="American",
+#     famous_for="Microsoft Founder"
+# )
 
-tim_berners_lee = Programmer(
-    first_name="Tim",
-    last_name="Berners-Lee",
-    gender="M",
-    nationality="British",
-    famous_for="World Wide Web"
-)
+# tim_berners_lee = Programmer(
+#     first_name="Tim",
+#     last_name="Berners-Lee",
+#     gender="M",
+#     nationality="British",
+#     famous_for="World Wide Web"
+# )
 
-dominic_quail = Programmer(
-    first_name="Dominic",
-    last_name="Quail",
-    gender="M",
-    nationality="South African / Irish",
-    famous_for="Thoughtful and unique design"
-)
+# dominic_quail = Programmer(
+#     first_name="Dominic",
+#     last_name="Quail",
+#     gender="M",
+#     nationality="South African / Irish",
+#     famous_for="Thoughtful and unique design"
+# )
 
 
 # Add instances of programmers to our session
@@ -115,32 +175,45 @@ dominic_quail = Programmer(
 #     session.commit()
 
 # Deleting a single record
-fname = input("Enter a first name: ")
-lname = input("Enter a last name: ")
-programmer = session.query(Programmer).filter_by(first_name=fname, 
-                                                 last_name=lname).first()
-# Defensive Programming
-if programmer is not None:
-    print("Programmer Found: ", programmer.first_name + " " + 
-          programmer.last_name)
-    confirmation = input("Are you sure you want to delete this record? (y/n) ")
-    if confirmation.lower() == "y":
-        session.delete(programmer)
-        session.commit()
-        print("Programmer has been deleted")
-    else:
-        print("Programmer not deleted")
-else:
-    print("No records found")
+# fname = input("Enter a first name: ")
+# lname = input("Enter a last name: ")
+# programmer = session.query(Programmer).filter_by(
+#              first_name=fname, last_name=lname).first()
+# # Defensive Programming
+# if programmer is not None:
+#     print("Programmer Found: ", programmer.first_name + " " +
+#           programmer.last_name)
+#     confirmation = input("Are you sure you want to" +
+#                          " delete this record? (y/n) ")
+#     if confirmation.lower() == "y":
+#         session.delete(programmer)
+#         session.commit()
+#         print("Programmer has been deleted")
+#     else:
+#         print("Programmer not deleted")
+# else:
+#     print("No records found")
 
-# Qeury the DB to find all programmers
-programmers = session.query(Programmer)
-for programmer in programmers:
+# Query the DB to find all programmers
+# programmers = session.query(Programmer)
+# for programmer in programmers:
+#     print(
+#         programmer.id,
+#         programmer.first_name + " " + programmer.last_name,
+#         programmer.gender,
+#         programmer.nationality,
+#         programmer.famous_for,
+#         sep=" | "
+#     )
+
+# Query the DB to get the car records
+cars = session.query(FaveCars)
+for car in cars:
     print(
-        programmer.id,
-        programmer.first_name + " " + programmer.last_name,
-        programmer.gender,
-        programmer.nationality,
-        programmer.famous_for,
+        car.CarId,
+        car.Make,
+        car.Model,
+        car.Engine,
+        car.BHP,
         sep=" | "
     )
